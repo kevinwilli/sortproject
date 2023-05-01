09ffdcba42ccbf3d1ad22e0a800be33bc542ad8b
kevinwilli <kbuikpor@guilford.edu>
2023-05-01 13:19:40 -0400

    add sort and search methods

--- src/main/java/edu/guilford/Car.java
+++ src/main/java/edu/guilford/Car.java
@@ -3,19 +3,21 @@ package edu.guilford;
 import java.util.Random;
 
 public class Car {
-    //attribute of the car
+    // attribute of the car
     private String make;
     private String model;
     private int year;
     private String color;
     private int price;
-    
+
     // constructor for the car
     public Car() {
         // array of makes, models, and colors
-        String[] makes = {"Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Mercedes-Benz", "Nissan", "Volkswagen", "Tesla", "Audi"};
-        String[] models = {"Corolla", "Civic", "F-150", "Silverado", "3 Series", "E-Class", "Altima", "Jetta", "Model S", "A4"};
-        String[] colors = {"Red", "Blue", "Green", "Yellow", "Black", "White", "Silver", "Gray"};
+        String[] makes = { "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Mercedes-Benz", "Nissan", "Volkswagen",
+                "Tesla", "Audi" };
+        String[] models = { "Corolla", "Civic", "F-150", "Silverado", "3 Series", "E-Class", "Altima", "Jetta",
+                "Model S", "A4" };
+        String[] colors = { "Red", "Blue", "Green", "Yellow", "Black", "White", "Silver", "Gray" };
         Random rand = new Random();
         // randomizes the make, model, year, color, and price of the car
         this.make = makes[rand.nextInt(makes.length)];
@@ -24,8 +26,9 @@ public class Car {
         this.color = colors[rand.nextInt(colors.length)];
         this.price = rand.nextInt(20000) + 10000;
     }
-    //getters for the car
-    //we don't need setters because we don't want to change the attributes of the car
+    // getters for the car
+    // we don't need setters because we don't want to change the attributes of the
+    // car
 
     public String getMake() {
         return make;
@@ -46,11 +49,12 @@ public class Car {
     public int getPrice() {
         return price;
     }
-    //toString method for the car to print of the car's attributes
+
+    // toString method for the car to print of the car's attributes
     @Override
     public String toString() {
         return year + " " + make + " " + model + ", " + color + ", $" + price;
     }
-}
 
-
+ 
+    }
--- src/main/java/edu/guilford/CarDriver.java
+++ src/main/java/edu/guilford/CarDriver.java
@@ -1,18 +1,20 @@
 package edu.guilford;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
 public class CarDriver {
     public static void main(String[] args) {
-        //instantiating the scanner and random objects
+        // instantiating the scanner and random objects
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
         System.out.print("How many cars do you want to generate? ");
         int numCars = scanner.nextInt();
-        //creating an array of cars with the size of numCars and filling it with random cars
+        // creating an array of cars with the size of numCars and filling it with random
+        // cars
         Car[] cars = new Car[numCars];
         for (int i = 0; i < numCars; i++) {
             cars[i] = new Car();
@@ -42,51 +44,87 @@ public class CarDriver {
         long selectionSortTime = System.nanoTime() - startTime;
         System.out.println("Selection Sorted Cars:");
         for (Car car : selectionSortedCars) {
-            System.out.println(car+"\n");
+            System.out.println(car + "\n");
         }
         System.out.println("Selection Sort Time: " + selectionSortTime + " nanoseconds");
-     
-       //add a static method to sort the car
-        Car[] insertionSortedCars = Arrays.copyOf(cars, numCars);
+        System.out.println();
+
+        // merge sort
+        Car[] mergeSortedCars = Arrays.copyOf(cars, numCars);
         startTime = System.nanoTime();
-        for (int i = 1; i < insertionSortedCars.length; i++) {
-            Car key = insertionSortedCars[i];
-            int j = i - 1;
-            while (j >= 0 && insertionSortedCars[j].getPrice() > key.getPrice()) {
-                insertionSortedCars[j + 1] = insertionSortedCars[j];
-                j = j - 1;
-            }
-            insertionSortedCars[j + 1] = key;
+        mergeSort(mergeSortedCars, 0, mergeSortedCars.length - 1);
+        long mergeSortTime = System.nanoTime() - startTime;
+        System.out.println("Merge Sorted Cars:");
+        for (Car car : mergeSortedCars) {
+            System.out.println(car + "\n");
         }
-        long insertionSortTime = System.nanoTime() - startTime;
-        System.out.println("Insertion Sorted Cars:");
-        for (Car car : insertionSortedCars) {
-            System.out.println(car+"\n");
-        }
-        System.out.println("Insertion Sort Time: " + insertionSortTime + " nanoseconds");
-        
-        //add a static method to sort the car
-        Car[] bubbleSortedCars = Arrays.copyOf(cars, numCars);
-        startTime = System.nanoTime();
-        for (int i = 0; i < bubbleSortedCars.length - 1; i++) {
-            for (int j = 0; j < bubbleSortedCars.length - i - 1; j++) {
-                if (bubbleSortedCars[j].getPrice() > bubbleSortedCars[j + 1].getPrice()) {
-                    // swap temp and arr[i]
-                    Car temp = bubbleSortedCars[j];
-                    bubbleSortedCars[j] = bubbleSortedCars[j + 1];
-                    bubbleSortedCars[j + 1] = temp;
-                }
-            }
-        }
-        long bubbleSortTime = System.nanoTime() - startTime;
-        System.out.println("Bubble Sorted Cars:");
-        for (Car car : bubbleSortedCars) {
-            System.out.println(car+"\n");
-        }
-        System.out.println("Bubble Sort Time: " + bubbleSortTime + " nanoseconds");
-        
-       
+        System.out.println("Merge Sort Time: " + mergeSortTime + " nanoseconds");
+        System.out.println();
+
     }
 
-    
+    private static void mergeSort(Car[] mergeSortedCars, int i, int j) {
+        if (i < j) {
+            int mid = (i + j) / 2;
+            mergeSort(mergeSortedCars, i, mid);
+            mergeSort(mergeSortedCars, mid + 1, j);
+            merge(mergeSortedCars, i, mid, j);
+        }
+    }
+
+    private static void merge(Car[] mergeSortedCars, int i, int mid, int j) {
+        Car[] temp = new Car[j - i + 1];
+        int left = i;
+        int right = mid + 1;
+        int k = 0;
+        while (left <= mid && right <= j) {
+            if (mergeSortedCars[left].getPrice() <= mergeSortedCars[right].getPrice()) {
+                temp[k] = mergeSortedCars[left];
+                left++;
+            } else {
+                temp[k] = mergeSortedCars[right];
+                right++;
+            }
+            k++;
+        }
+        while (left <= mid) {
+            temp[k] = mergeSortedCars[left];
+            left++;
+            k++;
+        }
+        while (right <= j) {
+            temp[k] = mergeSortedCars[right];
+            right++;
+            k++;
+        }
+        for (k = 0; k < temp.length; k++) {
+            mergeSortedCars[i + k] = temp[k];
+        }
+
+        //binary search
+        int searchPrice = 15000;
+        int index = binarySearch(mergeSortedCars, searchPrice);
+        if (index == -1) {
+            System.out.println("No car found with price $" + searchPrice);
+        } else {
+            System.out.println("Car found with price $" + searchPrice + ": " + mergeSortedCars[index]);
+        }
+    }
+
+    private static int binarySearch(Car[] mergeSortedCars, int searchPrice) {
+        int left = 0;
+        int right = mergeSortedCars.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mergeSortedCars[mid].getPrice() == searchPrice) {
+                return mid;
+            } else if (mergeSortedCars[mid].getPrice() < searchPrice) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
+
 }
